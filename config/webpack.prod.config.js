2f9bd9eb71a191825ebaed08fc6860d6f525a133
Alex <aafu@ucdavis.edu>
2019-04-20 13:47:49 -0400

    Add some webpack configs but will probably fix at some point

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,5 +1,7 @@
 const TerserPlugin = require('terser-webpack-plugin');
 const webpack = require('webpack');
+const ChunkManifestPlugin = require('chunk-manifest-webpack-plugin');
+const WebpackChunkHash = require('webpack-chunk-hash');
 
 const config = {
   mode: 'production',
@@ -7,9 +9,22 @@ const config = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
-    })
+    }),
+    // new webpack.HashedModuleIdsPlugin(),
+    // new WebpackChunkHash(),
+    // new ChunkManifestPlugin({
+    //   filename: 'chunk-manifest.json',
+    //   manifestVariable: 'webpackManifest',
+    //   inlineManifest: true,
+    // }),
   ],
+  // output: {
+  //   filename: '[name].[chunkhash].js'
+  // },
   optimization: {
+    splitChunks: {
+      chunks: "initial",
+    },
     minimizer: [new TerserPlugin({
       sourceMap: true,
       parallel: true,
